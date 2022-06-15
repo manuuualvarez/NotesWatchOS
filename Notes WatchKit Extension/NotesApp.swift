ef39360b44e472fd4cd53a5744b721ddae45f7ef
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2022-06-15 17:19:53 -0300

    Initial Commit

---
+++ Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesApp.swift
+//  Notes WatchKit Extension
+//
+//  Created by Manny Alvarez on 15/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
